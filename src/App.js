69cb48db9c0c05c25124d42aa9b60798e43c6b41
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-08-13 12:11:54 +0100

    added logos for light and dark mode

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { Button, ButtonBase, IconButton, ThemeProvider, createTheme } from '@mui/material';
 
 
-
 function App() {
 
   const [loggedIn, setLoggedIn] = useState(false);
@@ -52,7 +51,7 @@ function App() {
       <BrowserRouter>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <div className="App">
-          <div className=' bg-white dark:bg-[#202124] transition-all duration-300'>
+          <div className=' bg-white dark:bg-[#202124] transition-all duration-100'>
             <Header loggedIn={loggedIn} darkMode={darkMode} handleThemeSwitch={handleThemeSwitch}/>
 
             <div className='w-[100vw] h-[100vh] flex-col flex justify-center p-0'>
