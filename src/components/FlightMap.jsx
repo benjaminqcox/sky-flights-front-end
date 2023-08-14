feaedeec9153a7e90c56bbd416b568671487b139
benjaminqcox <benjaminqcox@gmail.com>
2023-08-14 18:48:46 +0100

    Added icao to coordinates to points on a map

--- src/components/FlightMap.jsx
+++ src/components/FlightMap.jsx
@@ -1,35 +1,50 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import axios from "axios";
 
-const AirportCoords = [[0.4551, 51.4680], [12.2519, 41.8034], [73.7797, 40.6446]]
+const AirportICAOCodes = ["KJFK", "EGLL"]
 
 function lngLatToLatLng(lngLat) {
     return [lngLat[1], lngLat[0]];
 }
 
+const icaoToCoords = async (icao) => {
+    var config = {
+      method: 'get',
+      url: 'https://api.checkwx.com/metar/' + icao + '/decoded',
+      headers: { 'X-API-Key': 'c4aeb4c15f084c32996d1c55e2' }
+    };
+    
+    const res = await axios(config);
+    console.log(res.data.data[0].station.geometry.coordinates);
+    return res.data.data[0].station.geometry.coordinates;   
+
+}
+
 const FlightMap = () => {
+    const [allCoords, setAllCoords] = useState([]);
+
+    useEffect(() => {
+        //need to sort this to get the coords from icao, its almost there
+        const getCoords = async () => {
+            return Promise.all(AirportICAOCodes.map((code) => icaoToCoords(code)))
+        }
+        getCoords().then(data => setAllCoords(data));    
+        
+    }, []);
     return (
         <>
-            <MapContainer center={lngLatToLatLng(AirportCoords[0])} zoom={12} scrollWheelZoom={false}>
-            {AirportCoords.map((coords) => (
-                    <Marker
-                        key={coords[0]}
-                        position={[
-                            coords[1],
-                            coords[0]
-                        ]}
-                        onClick={() => {
-                            console.log(coords)
-                        }}
-                    />
-                    )
+            {allCoords.length &&
+                <MapContainer center={lngLatToLatLng(allCoords[0])} zoom={12} scrollWheelZoom={false}>
+                {allCoords.map((coordPair) => <Marker key={coordPair[0]} position={lngLatToLatLng(coordPair)}/>
                 )}
-                <Polyline positions={AirportCoords.map((coordPair) => lngLatToLatLng(coordPair))}/>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </MapContainer>
+                    <Polyline positions={allCoords.map((coordPair) => lngLatToLatLng(coordPair))}/>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </MapContainer>
+            }
         </>
     )
 }
