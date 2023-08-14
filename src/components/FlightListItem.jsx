40721e26140a3c84b29c08ee12b0c71dce508fb1
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-08-14 20:31:53 +0100

    flight list item component changes with passed in data dynamically

--- src/components/FlightListItem.jsx
+++ src/components/FlightListItem.jsx
@@ -7,16 +7,21 @@ import Favorite from '@mui/icons-material/Favorite';
 import Checkbox from '@mui/material/Checkbox';
 import AddIcon from '@mui/icons-material/Add';
 import { useState } from 'react';
+import { dayCalendarSkeletonClasses } from '@mui/x-date-pickers';
 
 function FlightListItem( {darkMode, flightData} ) {
     const [moreInfo, setMoreInfo] = useState(false);
     // <button onClick={() => setMoreFilters(!moreFilters)} className='box-border text-white h-[57px] font-semibold border-[1px] rounded-lg px-3 hover:bg-opacity-100 dark:hover:bg-opacity-100 border-blue-500 dark:border-blue-700 hover:text-white shadow-black hover:shadow-md bg-blue-500 dark:bg-blue-700 transition-all duration-200 active:brightness-[80%] active:shadow-none active:translate-y-[1px]'>{moreFilters? "- Less Filters" : "+ More Filters"}</button>
 
+    const departureDate = flightData.local_departure.substring(0, flightData.local_departure.indexOf("T"));
+    const departureTime = flightData.local_departure.substring(flightData.local_departure.indexOf("T") + 1, flightData.local_departure.indexOf("Z"));
+    const arrivalDate = flightData.local_arrival.substring(0, flightData.local_arrival.indexOf("T"));
+    const arrivalTime = flightData.local_departure.substring(flightData.local_arrival.indexOf("T") + 1, flightData.local_arrival.indexOf("Z"));
     
     return ( 
         <>
             <div className={`text-gray-600 place-items-stretch dark:text-gray-300 shadow-lg justify-items-center border-slate-300/30 dark:border-slate-100/10 rounded-3xl border-[1px] w-[1000px] max-w-[90%] h-[80vh] sm:h-[20vw] min-h-[330px] transition-all duration-300 ease-out mt-5 flex flex-col sm:flex-row mx-auto   ${moreInfo ? `min-h-[50%]` : `min-h-[25%]`} min-h-[230px] mx-auto transition-all duration-300 ease-out mt-28 sm:mt-0 overflow-scroll`}>
-                <div className='p-5 text-left border-0 border-solid border-black sm:w-[37%]'>
+                <div className='p-5 text-left boSßSrder-0 border-solid border-black sm:w-[37%]'>
                     <p className='text-xl'>
                       Emirates  
                     </p>
@@ -27,20 +32,21 @@ function FlightListItem( {darkMode, flightData} ) {
                 <div className='border-0 border-solid border-black sm:w-[37%] pt-7 pl-5'>
                     <MantineProvider theme={{ colorScheme: `${darkMode === "dark" ? 'dark' : 'light'}` }}>
                         <Timeline active={4} bulletSize={24} lineWidth={2}>
-                            <Timeline.Item bullet={<IconGitBranch size={12} />} title="London Heathrow Airport LHR">
-                                <Text color="dimmed" size="sm">16:00<Text variant="link" component="span" inherit></Text></Text>
-                                <Text size="xs" mt={4}>2 hours left</Text>
+                            <Timeline.Item bullet={<IconGitBranch size={12} />} title={`${flightData.cityFrom + ' ' + flightData.flyFrom}`}>
+                                <Text color="dimmed" size="sm">{departureTime.substring(0, 5)}<Text variant="link" component="span" inherit></Text></Text>
+                                <Text size="xs"ß mt={4}>{Math.round(flightData.duration / 3600) + ' hours'}</Text>
                             </Timeline.Item>
 
-                            <Timeline.Item bulletSize={12} in bullet={<IconGitCommit size={12}/>} title="Paris Charles de Gaule CDG"  lineVariant="dashed">
+                            {/* <Timeline.Item bulletSize={12} in bullet={<IconGitCommit size={12}/>} title="Paris Charles de Gaule CDG"  lineVariant="dashed">
                                 <Text color="dimmed" size="sm">18:00<Text variant="link" component="span" inherit></Text></Text>
                             </Timeline.Item>
                             <Timeline.Item bulletSize={12} in bullet={<IconGitCommit size={12}/>} title="Paris Charles de Gaule CDG" >
                                 <Text color="dimmed" size="sm">18:00<Text variant="link" component="span" inherit></Text></Text>
                                 <Text size="xs" mt={4}>4 hours left</Text>
-                            </Timeline.Item>
-                            <Timeline.Item bullet={<IconGitCommit size={12} />} title="India Gandhi International Airport DEL">
-                                <Text color="dimmed" size="sm">18:00<Text variant="link" component="span" inherit></Text></Text>
+                            </Timeline.Item> */}
+
+                            <Timeline.Item bullet={<IconGitCommit size={12} />} title={`${flightData.cityTo} + ${flightData.flyTo}`}>
+                                <Text color="dimmed" size="sm">{arrivalTime.substring(0, 5)}<Text variant="link" component="span" inherit></Text></Text>
                             </Timeline.Item>
                         </Timeline>
                     </MantineProvider>
@@ -55,7 +61,7 @@ function FlightListItem( {darkMode, flightData} ) {
                     }}/>
                     </div>
                     <p className='border-0 border-solid font-bold border-black text-2xl'>
-                      £131  
+                      £{flightData.fare.adults}  
                     </p>
 
                     <p className='border-0 border-solid border-black'>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchForm from './SearchForm';
 import FlightListItem from './FlightListItem';
 import dummyFlightData from '../dummyFlightData.json'
+import axios from 'axios';
 
 function LandingPage({darkMode}) {
 
-    const [flights, setFlights] = useState([])
+    const [listOfFlights, setListOfFlights] = useState([]);
+
+    useEffect(() => {
+        getFlights();
+    }, []);
+
+    const getFlights = async () => {
+        try {
+            const URL = `http://localhost:8081/booking/getFiltered/?flyTo=JFK&flyFrom=LHR&leaveDateFrom=30/08/2023&leaveDateTo=30/08/2023`;
+            const response = await axios.get(URL);
+            setListOfFlights(response.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    console.log(listOfFlights)
 
     return ( 
         <div className='w-[100vw] h-[100vh] -mt-[80px] flex-col flex justify-start p-0'>
             <SearchForm darkMode={darkMode}/>
 
-            {dummyFlightData.map((data) => {
-            console.log(data)
-            return <FlightListItem darkMode={darkMode} flightData={data}/>
+            {listOfFlights.map((flight) => {
+            return <FlightListItem darkMode={darkMode} flightData={flight}/>
             })}
 
             {/* <Button className='w-[50%]'>Hello</Button> */}
