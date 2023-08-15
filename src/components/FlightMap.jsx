2e32861252292a09d63170b3b8e09f2c0d72cd4f
benjaminqcox <benjaminqcox@gmail.com>
2023-08-15 10:12:26 +0100

    added for the iata code

--- src/components/FlightMap.jsx
+++ src/components/FlightMap.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from "react";
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
-import axios from "axios";
+import axios, { all } from "axios";
 
-const AirportICAOCodes = ["KJFK", "EGLL", "LIRF"]
+const AirportICAOCodes = ["KJFK", "EGLL", "LIRF"];
+const AirportIATACodes = ["DEL", "LHR", "CDG"];
 
 function lngLatToLatLng(lngLat) {
     return [lngLat[1], lngLat[0]];
@@ -16,29 +17,40 @@ const icaoToCoords = async (icao) => {
     };
     
     const res = await axios(config);
-    console.log(res.data.data[0].station.geometry.coordinates);
     return res.data.data[0].station.geometry.coordinates;   
 
 }
 
+const iataToCoords = async (iata) => {
+    // Value of Token
+    const token = 'Gmss-7DE9NkgyWvWK7k9oDHg-q9gn7c8YbTg3U2SPrA'
+    const resp = await axios.get('https://avwx.rest/api/station/' + iata, {
+        headers: {
+            Authorization: 'BEARER ' + token
+        }
+    })
+    const data = resp.data;
+    return([data.latitude, data.longitude]);
+}
+
 const FlightMap = () => {
-    const [allCoords, setAllCoords] = useState([]);
+    const [allLatLng, setAllLatLng] = useState([]);
 
     useEffect(() => {
         //need to sort this to get the coords from icao, its almost there
-        const getCoords = async () => {
-            return Promise.all(AirportICAOCodes.map((code) => icaoToCoords(code)))
+        const getLatLng = async () => {
+            return Promise.all(AirportIATACodes.map((code) => iataToCoords(code)))
         }
-        getCoords().then(data => setAllCoords(data));    
+        getLatLng().then(data => setAllLatLng(data));
         
     }, []);
     return (
         <>
-            {allCoords.length &&
-                <MapContainer center={lngLatToLatLng(allCoords[0])} zoom={12} scrollWheelZoom={false}>
-                {allCoords.map((coordPair) => <Marker key={coordPair[0]} position={lngLatToLatLng(coordPair)}/>
+            {allLatLng.length &&
+                <MapContainer center={allLatLng[0]} zoom={12} scrollWheelZoom={false}>
+                {allLatLng.map((latLng) => <Marker key={latLng[0]} position={latLng}/>
                 )}
-                    <Polyline positions={allCoords.map((coordPair) => lngLatToLatLng(coordPair))}/>
+                    <Polyline positions={allLatLng}/>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
