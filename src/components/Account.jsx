0fbb5e7518607f6a096f5fc061d4cc7153d68e07
benjaminqcox <benjaminqcox@gmail.com>
2023-08-17 19:28:03 +0100

    added base bookings page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -23,7 +23,7 @@ function Account ({ setLoggedIn, user, darkMode }){
     return (
         <div className={`bg-white dark:bg-[#202124] text-gray-600 place-items-stretch dark:text-gray-300 shadow-2xl justify-items-center border-slate-300/30 dark:border-slate-100/10 rounded-3xl border-[1px] w-[500px] max-w-[70%] h-[30vh] sm:h-[30vh] max-h-[200px] transition-all duration-300 ease-out mt-5 flex flex-col p-5 gap-3 mx-auto absolute right-5 top-[80px] z-50`}>
             <button className="sm:text-2xl font-['Roboto'] font-light" onClick={handleLogout}>Logout</button>
-            <button className="sm:text-2xl font-['Roboto'] font-light" onClick={() => navigate('/bookings')}>Bookings</button>
+            <button className="sm:text-2xl font-['Roboto'] font-light" onClick={() => navigate('/bookings', {user})}>Bookings</button>
         </div>
         
     )
--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -2,17 +2,25 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Booking = ({user}) => {
+    console.log(user)
     const [bookings, setBookings] = useState([]);
 
-    const getUserID = async () => {
-        const URL = `http://localhost:8080/user/getUserID/user` + user;
+    const getUserName = async () => {
+        const URL = `http://localhost:8080/users/user/`;
         const response = await axios.get(URL, { withCredentials: true });
         return response.data;
     }
 
+    const getUserID = async () => {
+        const user = await getUserName();
+        const URL = `http://localhost:8080/users/getUserID/` + user;
+        const response = await axios.get(URL, { withCredentials: true });
+        return response.data.data;
+    }
+
     const getBookings = async () => {
-        const userID = await getUserID();
-        const URL = `http://localhost:8080/booking/getByUserID/` + userID;
+        const userID = await getUserName();
+        const URL = `http://localhost:8080/booking/getByUsername/` + userID;
         const response = await axios.get(URL, { withCredentials: true });
         setBookings(response.data);
     }
@@ -20,6 +28,7 @@ const Booking = ({user}) => {
     useEffect(() => {
         getBookings();
     }, []);
+    console.log(bookings)
 
     return (
         <>
