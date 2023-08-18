2ca1a0d32c566cbc3cbfa30989d9219a9b0f52af
benjaminqcox <61159657+benjaminqcox@users.noreply.github.com>
2023-08-18 13:01:32 +0100

    Update Bookings.jsx

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -12,21 +12,21 @@ const Booking = () => {
     const [toDelete, setToDelete] = useState();
 
     const getUserName = async () => {
-        const URL = `http://3.9.218.228:8081/users/user/`;
+        const URL = `/api/users/user/`;
         const response = await axios.get(URL, { withCredentials: true });
         return response.data;
     }
 
     const getUserID = async () => {
         const user = await getUserName();
-        const URL = `http://3.9.218.228:8081/users/getUserID/` + user;
+        const URL = `/api/users/getUserID/` + user;
         const response = await axios.get(URL, { withCredentials: true });
         return response.data.data;
     }
 
     const getBookings = async () => {
         const userName = await getUserName();
-        const URL = `http://3.9.218.228:8081/users/getByUsername/` + userName;
+        const URL = `/api/users/getByUsername/` + userName;
         const response = await axios.get(URL, { withCredentials: true });
         setBookings(response.data.bookings);
     }
